0762fbc82967a9ece213a16af1212619dea0e4f1
casey829 <caseymwangi31@gmail.com>
2025-01-29 23:10:41 +0300

    making the movies align in a row

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard'
 import { useDebounce } from 'react-use'
+import { getTrendingMovies, updateSearchCount } from './appwrite'
 
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
@@ -25,6 +26,8 @@ function App() {
   const [errorMessage, setErrorMessage] = useState('');
 
   const [movieList, setMovieList] = useState([]);
+
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -55,6 +58,9 @@ function App() {
 
     setMovieList(data.results);
 
+   if(query && data.results.length > 0){
+     await updateSearchCount(query, data.results[0])
+   }
     if(data.Response === 'False'){
         setErrorMessage(data.Error || 'Failed to fetch movies');
     }
@@ -68,10 +74,24 @@ function App() {
 
   }
 
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`error fetching trending movies: ${error}`);
+  
+    }
+  }
+
 useEffect(()=> {
   fetchMovies(debouncedSearchMovie)
 }, [debouncedSearchMovie])
 
+useEffect(()=> {
+  loadTrendingMovies();
+}, [])
   return (
     
    <main>
@@ -87,19 +107,35 @@ useEffect(()=> {
           <Search  searchMovie={searchMovie} setSearchMovie={setSearchMovie}/>
           <h1 className='text-white'>{searchMovie}</h1>
         </header>
-        <section>
+       {trendingMovies.length > 0 &&(
+        <section className='trending'>
+          <h2 className='text-white'>Trending Movies</h2>
+          <ul>
+            {trendingMovies.map((movie, index) => (
+              <li key={movie.$id}>
+            <p>{index + 1}</p>
+            <img src={movie.poster_url}
+              alt={movie.title}
+            />
+              </li>
+            ))}
+          </ul>
+   
+        </section>
+       )}
 
-          <h2 className='mt-[40px]'>All Movies</h2>
+        <section>
+          <h2 >All Movies</h2>
           {isLoading ? (
             <Spinner />
           ) : errorMessage ? (
             <p>{errorMessage}</p>
           ) : (
-            <ul>
-              {movieList.map((movie) => (
-               <MovieCard key={movie.id} movie={movie}/>
-              ))}
-            </ul>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {movieList.map((movie) => (
+              <MovieCard key={movie.id} movie={movie}/>
+            ))}
+          </div>
           )}   
         </section>
       </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,34 +4,33 @@ const MovieCard = ({movie: {
     title,vote_average,poster_path, release_date, original_language
 }}) => {
   return (
-    <div className='movie-card'>
-    <img 
-      src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}`: '/no-movie.png'}
-      alt='title'
-    />
-    <div className='mt-4'>
-        <h3>{title}</h3>
-    </div>
-
-    <div className='content'>
-      <div className='ratinf'>
-       <img 
-       src='./images/star.svg'
-       alt='Star Icon'
-       />
-       <p className='text-white'>{vote_average ? vote_average.toFixed(1):'N/A' }</p>
+    <div>
+      <img 
+        src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}`: '/no-movie.png'}
+        alt='title'
+      />
+      <div className='mt-4'>
+          <h3 className='text-white'>{title}</h3>
       </div>
-      <span>•</span>
-      <p className='lang'>{original_language}</p>
 
-      <span>•</span>
-      <p className='year'>
-        {release_date ? release_date.split('-')[0] : 'N/A'}
-      </p>
-    </div>
+      <div className='content'>
+        <div className='ratinf'>
+         <img 
+         src='./images/star.svg'
+         alt='Star Icon'
+         />
+         <p className='text-white'>{vote_average ? vote_average.toFixed(1):'N/A' }</p>
+        </div>
+        <span>•</span>
+        <p className='lang'>{original_language}</p>
+
+        <span>•</span>
+        <p className='year text-white '>
+          {release_date ? release_date.split('-')[0] : 'N/A'}
+        </p>
+      </div>
     </div>
   )
-  
 }
 
 export default MovieCard
